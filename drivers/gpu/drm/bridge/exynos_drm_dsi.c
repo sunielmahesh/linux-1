2837344a47a8035d1bdab1ef24ab04cddccf3df7
Jagan Teki <jagan@amarulasolutions.com>
2021-06-30 23:00:20 +0530

    drm: bridge: exynos_drm_dsi: Add DE polarity timings
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/gpu/drm/bridge/exynos_drm_dsi.c
+++ drivers/gpu/drm/bridge/exynos_drm_dsi.c
@@ -1681,6 +1681,10 @@ static const struct component_ops exynos_dsi_component_ops = {
 	.unbind	= exynos_dsi_unbind,
 };
 
+static const struct drm_bridge_timings exynos_dsi_bridge_timings = {
+	.input_bus_flags = DRM_BUS_FLAG_DE_LOW,
+};
+
 static int exynos_dsi_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -1777,6 +1781,7 @@ static int exynos_dsi_probe(struct platform_device *pdev)
 
 	dsi->bridge.funcs = &exynos_dsi_bridge_funcs;
 	dsi->bridge.of_node = dev->of_node;
+	dsi->bridge.timings = &exynos_dsi_bridge_timings;
 	dsi->bridge.type = DRM_MODE_CONNECTOR_DSI;
 
 	drm_bridge_add(&dsi->bridge);
